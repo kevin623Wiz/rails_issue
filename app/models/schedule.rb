6157a86c0ad973636d5372ea6d30c7abe1df7d5e
kevin623Wiz <tamagoball10@gmail.com>
2023-07-06 20:50:10 +0900

    時間表示の調整

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,2 +1,6 @@
 class Schedule < ApplicationRecord
+    validates :title, presence: true,length: {maximum: 20}
+    validates :start, presence: true
+    validates :finish, presence: true
+    validates :memo, length: {maximum: 500}
 end
--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,2 +1,1 @@
 Time::DATE_FORMATS[:datetime_jp] = '%Y/%m/%d %H:%m'
-Time::DATE_FORMATS[:datetime_jp2] = '%Y/%m/%d'
